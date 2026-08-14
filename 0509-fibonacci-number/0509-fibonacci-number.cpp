7ea5f6ca41e4e4c1b837cd00bc76816aa3cb7a42
Khush Jain <khushjain.2901@gmail.com>
2026-08-14 19:43:04 +0530

    Time: 0 ms (100%), Space: 7.6 MB (89.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        else if(n==1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+        // if(n==0){
+        //     return 0;
+        // }
+        // else if(n==1){
+        //     return 1;
+        // }
+        // return fib(n-1)+fib(n-2);
+        int a=0,b=1;
+
+         if(n==0){
+             return a;
+         }
+         else if(n==1){
+             return b;
+         }
+         
+
+         for(int i=2;i<=n;i++){
+            int sum=a+b;
+            a=b;
+            b=sum;
+         }
+         return b;
+         
     }
 };
