7f44af49cc3893d61421251974ef42dc62e1ca8f
Khush Jain <khushjain.2901@gmail.com>
2026-08-27 19:05:10 +0530

    Time: 43 ms (49.19%), Space: 85.7 MB (91.84%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
+        if(nums.size()<2) return 0;
         int gap=0;
         sort(nums.begin(),nums.end());
         for(int i=1;i<nums.size();i++){
