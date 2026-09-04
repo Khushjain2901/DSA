38bf4cd9e321e2d7193de0c8dff60473dbeb1413
Khush Jain <khushjain.2901@gmail.com>
2026-09-04 21:21:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (16.85%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int open=0,close=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                open++;
-            }
-            else{
-                if(open>0){
-                    open--;
-                }
-                else{
-                    close++;
-                }
-            }
+        // int open=0,close=0;
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]=='('){
+        //         open++;
+        //     }
+        //     else{
+        //         if(open>0){
+        //             open--;
+        //         }
+        //         else{
+        //             close++;
+        //         }
+        //     }
+        // }
+        // return close+open;
+
+
+        stack<char>st;
+int ans=0;
+for(int i=0;i<s.size();i++){
+    if(s[i]=='('){
+        st.push(s[i]);
+    }
+    else{
+        if(st.empty()){
+            ans++;
         }
-        return close+open;
+        else{
+            st.pop();
+        }
+    }
+}
+
+return st.size()+ans;
     }
 };
