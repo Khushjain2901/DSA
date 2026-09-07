7816c199728cb776c694b68be8a14542288b2015
Khush Jain <khushjain.2901@gmail.com>
2026-09-07 16:32:26 +0530

    Time: 28 ms (28.73%), Space: 107.3 MB (34.97%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -5,16 +5,14 @@ public:
         int n= temperatures.size();
         vector<int>ans(n,0);
         for(int i=0;i<n;i++){
-            if(st.empty()){
-                st.push(i);
-            }
-            else{
+           
+            
                 while(!st.empty()&& temperatures[i]>temperatures[st.top()]){
                     ans[st.top()]=i-st.top();
                     st.pop();
                 }
                 st.push(i);
-            }
+            
         }
         return ans;
     }
