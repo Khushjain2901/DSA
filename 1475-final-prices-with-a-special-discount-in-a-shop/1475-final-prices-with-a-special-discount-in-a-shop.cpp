ef0c4b2bef0fb6cb734e5bd52ec8e47053e53778
Khush Jain <khushjain.2901@gmail.com>
2026-09-07 16:43:42 +0530

    Time: 3 ms (12.78%), Space: 14.3 MB (6.5%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n=prices.size();
+        vector<int>ans(n,0);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty()&&prices[st.top()]>=prices[i]){
+                int index=st.top();
+                prices[index]=prices[index]-prices[i];
+                st.pop();
+            }
+            st.push(i);
+
+        }
+        return prices;
+    }
+};
