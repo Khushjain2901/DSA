290cb7c69f8c831988c501caa273138fc18a6b42
Khush Jain <khushjain.2901@gmail.com>
2026-09-09 11:26:17 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,19 +1,54 @@
+// class Solution {
+// public:
+//     long long countCommas(long long n) {
+//         long long ans=0;
+//         long long start=1000;
+//         int commas=1;
+
+//         while(start<=n){
+//             long long end=min(n,start*1000-1);
+
+//             ans+=(end-start+1)*commas;
+
+//             start*=1000;
+//             commas++;
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long ans=0;
-        long long start=1000;
-        int commas=1;
-
-        while(start<=n){
-            long long end=min(n,start*1000-1);
-
-            ans+=(end-start+1)*commas;
-
-            start*=1000;
-            commas++;
+        if(n<1000){
+            return 0;
         }
-
-        return ans;
+        else if(n<1000000){
+            return n-999;
+        }
+        else if(n<1000000000LL){
+            return (1000000LL-1000)*1+
+                   (n-1000000LL+1)*2;
+        }
+        else if(n<1000000000000LL){
+            return (1000000LL-1000)*1+
+                   (1000000000LL-1000000LL)*2+
+                   (n-1000000000LL+1)*3;
+        }
+        else if(n<1000000000000000LL){
+            return (1000000LL-1000)*1+
+                   (1000000000LL-1000000LL)*2+
+                   (1000000000000LL-1000000000LL)*3+
+                   (n-1000000000000LL+1)*4;
+        }
+        else{
+            return (1000000LL-1000)*1+
+                   (1000000000LL-1000000LL)*2+
+                   (1000000000000LL-1000000000LL)*3+
+                   (1000000000000000LL-1000000000000LL)*4+
+                   (n-1000000000000000LL+1)*5;
+        }
     }
 };
